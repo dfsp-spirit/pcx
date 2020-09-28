dba0d6876d4ae9b3dfff50c680901384ce13112f
Tim Schäfer <ts+code@rcmd.org>
2020-09-28 23:34:46 +0200

    read palette

--- R/read_pcx.R
+++ R/read_pcx.R
@@ -43,10 +43,13 @@ read.pcx <- function(filepath, hdr = TRUE) {
   header$screen_size_vertical = readBin(fh, integer(), n = 1, size = 2, endian = endian); # vertical
   header$reserved2 = readBin(fh, integer(), n = 54, size = 1, endian = endian);
 
-  pcx$header = header;
-
   img_width = header$maxx - header$minx + 1L;
   img_height = header$maxy - header$miny + 1L;
+
+  header$width = img_width;
+  header$height = img_height;
+
+  pcx$header = header;
 
   img_num_pixels = img_width * img_height;
   img_num_values = img_num_pixels * header$num_channels;
@@ -65,10 +68,10 @@ read.pcx <- function(filepath, hdr = TRUE) {
     for(j in 1:header$num_channels) {
       row_pixel_index = 1L;
       for(k in 1:header$bytes_per_channels_line) {
-        raw_value = readBin(fh, integer(), n = 1L, size = 1L);
+        raw_value = readBin(fh, integer(), n = 1L, size = 1L, signed = FALSE);
         if(raw_value > 192L) { # repeat
           repeat_times = raw_value - 192L;
-          repeat_color = readBin(fh, integer(), n = 1L, size = 1L);
+          repeat_color = readBin(fh, integer(), n = 1L, size = 1L, signed = FALSE);
           k = k + 1L;
           if(row_pixel_index <= img_width) { # in image data
             if(repeat_times > 0L) {
@@ -88,7 +91,26 @@ read.pcx <- function(filepath, hdr = TRUE) {
   }
 
   # check for palette
+  seek(fh, where = -(768L +1L), origin = "end");
+  palette_check = readBin(fh, integer(), n = 1L, size = 1L);
+  if(palette_check == 12L) {
+    pcx$header$has_palette_at_end = TRUE;
+    palette = array(rep(0L, (256 * 4L)), dim = c(256L, 4L));
+    for(i in 1:256) {
+      for(j in 1:3) {  # the 4th entry is 'reserved', it is NOT to be read from the file.
+        palette[i,j] = readBin(fh, integer(), n = 1L, size = 1L, signed = FALSE);
+      }
+    }
+    pcx$palette = palette;
+  } else {
+    pcx$header$has_palette_at_end = FALSE;
+  }
 
   pcx$data = img_data;
+  class(pcx) = c(class(pcx), 'pcx');
   return(pcx);
+}
+
+print.pcx <- function(x, ...) {
+  cat(sprintf("PCX bitmap image, dimension %d x %d pixels, %d channels.\n", x$header$width, x$header$height, x$header$num_channels));
 }
